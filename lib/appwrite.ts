355620b0a3281676a1b22a45f4e0b9c2aaaa6266
Athul Kammath <athul.kammath@arithaconsulting.com>
2026-01-05 15:08:46 +0530

    Add Appwrite configuration with environment variables and URL polyfill, and update Babel plugins.

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwrite = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.aritha.foodapp",
+    projectID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
